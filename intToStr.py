cabbf2ca650b8c7489b60a2d6e0c09fce345c6c8
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-12 20:19:59 +0200

    signed integer

--- intToStr.py
+++ intToStr.py
@@ -5,15 +5,17 @@
 
 def int_to_str(n):
     s = [] 
+    sign = -1 if n<0 else 1
+    n = abs(n)
     while n>0:
         c = n%10
         s.append(chr(ord('0')+c))
         n = n//10
     s.reverse() 
-    print("".join(s))
-    
-    return "".join(s)
+    x = "".join(s) if sign>0 else ("-"+"".join(s)) 
+    return x 
 
 
+print(int_to_str(123))
 
-int_to_str(123)
+print(int_to_str(-9999))
