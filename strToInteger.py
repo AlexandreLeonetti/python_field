c75b2863842959015ca254cc2c47184f058de717
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-12 17:53:03 +0200

    strToInteger2

--- strToInteger.py
+++ strToInteger.py
@@ -3,9 +3,14 @@
 # enter string return integer
 
 def str_to_int(s):
+    sign = 1
+    if s[0]=="-":
+        sign = -1
+        s = s[1:]
     n = 0
     for c in s:
-        print(c)
+        #print(c)
         n = n*10 + int(c)
-    return n
+    return n*sign
 print(str_to_int("345"))
+print(str_to_int("-77345"))
