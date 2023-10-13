8b510a54263637caade2fd32a22bf30973267139
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-13 09:57:18 +0200

    5_2

--- 5_2_IncIntArr.py
+++ 5_2_IncIntArr.py
@@ -1,5 +1,7 @@
 # increment an integer of arbitrary size
 a = [3,4,6,7,3,5]
+b = [1,2,3,8,9,9]
+c = [9,9,9,9,9,9]
 
 def inc(n):
     for i in reversed(range(len(n))):
@@ -8,7 +10,14 @@ def inc(n):
         if x<9:
             x +=1
             n[i] = x
-            break
-        
+            return n
+        else:
+            n[i]=0
+    
+    # if array is full of 9, 
+    n.append(1)
+    n.reverse()
     return n 
 print(inc(a))
+print(inc(b))
+print(inc(c))
