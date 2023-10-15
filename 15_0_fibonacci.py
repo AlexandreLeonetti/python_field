023b81227818aefbf3e9f1daa267db2a049d21e2
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-15 14:09:51 +0200

    fibo cache

--- 15_0_fibonacci.py
+++ 15_0_fibonacci.py
@@ -3,4 +3,21 @@
 # then with cashe
 
 
+cache = {}
 
+def fibo(n):
+
+    if n <= 2:
+        return 1
+    elif n in cache:
+        return cache[n]
+    else:
+        result = fibo(n-1) + fibo(n-2)
+    
+    # cache the result before returning it.
+    cache[n] = result
+    return result
+
+
+
+print(fibo(90))
