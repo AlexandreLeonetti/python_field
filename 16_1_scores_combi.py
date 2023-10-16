7955c802d8896a1bc3b263a9dce1ff7bcb385a53
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-16 11:43:19 +0200

    dynamic

--- 16_1_scores_combi.py
+++ 16_1_scores_combi.py
@@ -1,3 +1,32 @@
 # this is program is able to calculate to allscore combinations
 # using matrix calculations
 
+
+scores = [2,3,7]
+
+def possible (scr, target):
+    matrix = [[1]+[0]*target for s in scr]
+    for i,s in enumerate(scr):
+                
+            if i == 0 :
+                 for j in range(1,target+1):
+                     if j>=s:
+                        matrix[i][j]=matrix[i][j-s]
+
+            if i>0:           
+                for j in range(1, target+1):
+                    if j>=s :
+                        matrix[i][j] = matrix[i-1][j] + matrix[i][j-s] 
+                    else:
+                        matrix[i][j] = matrix[i-1][j]
+
+    return matrix
+
+
+x =possible(scores,10)
+for c in x:
+    print(c)
+
+
+
+
