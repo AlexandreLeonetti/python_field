35e697256f960fc016b0ded98e778ac2a196007e
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-18 16:16:55 +0200

    heap2

--- 10b_heaps.py
+++ 10b_heaps.py
@@ -6,4 +6,21 @@ arr2 = [2,3,4,6,8,10,12,14,16]
 arr3 = [100,222,333,444,555,666,777]
 
 
+def merge(*args):
+    all_arrs = args
+    minHeap = []
+    result = []
+
+    # initialize array of iterator values
+    iters = [iter(x) for x in all_arrs]
+
+    #initialize minHeap
+
+    #pass all values to minHeap and append them to result
+
+
+    return  result
+
+
+print(merge(arr1, arr2, arr3))
 
