d38ac7464056f46cff66384be055bdc6faa1c2c2
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-19 13:03:08 +0200

    strategy

---
+++ algo/basic.py
@@ -0,0 +1,142 @@
+# https://raposa.trade/blog/use-python-to-trade-the-donchian-channel/
+# this program calculates donchian channel 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+
+def indicator(data: pd.DataFrame, period: int):
+      #print(data)
+      #data["highDon"] = data["High"] #data["High"].rolling(period).max()
+      #data["lowerDon"] = data["Low"]#.rolling(period).min()
+      #data["midDon"] = (data["highDon"] + data["lowerDon"]) / 2
+      #data["yes"] = 9 # this adds a column named "yes" each elements of value 9
+      return data
+
+#downloading the correct data from yahoo finance
+ticker = "LTC-USD"
+yfObj = yf.Ticker(ticker) #returns data as panda dataframe.
+data = yfObj.history(start="2023-10-01", end="2023-10-15").drop(
+            ["Volume", "Stock Splits"], axis=1)
+data = indicator(data, 10)
+print(data)
+data.tail()
+"""
+# plotting the donchian chart
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
+plt.figure(figsize=(12, 8))
+plt.plot(data["Close"], label="Close", linewidth=0.5)
+plt.plot(data["highDon"], label="Upper", c=colors[1], linewidth=0.5)
+plt.plot(data["lowerDon"], label="Lower", c=colors[4], linewidth=0.5)
+plt.plot(data["midDon"], label="Mid", c=colors[2], linestyle=":", linewidth=0.5)
+plt.fill_between(data.index, data["highDon"], data["lowerDon"], alpha=0.3,
+                 color=colors[1])
+
+plt.xlabel("Date")
+plt.ylabel("Price ($)")
+plt.title(f"Donchian Channels for {ticker}")
+plt.xticks(rotation=45)
+plt.legend()
+plt.show()
+"""
+
+# strategy :
+
+
+def strategy(data: pd.DataFrame, period: int=10, shorts: bool=True):
+  data = indicator(data, period)
+
+  data["position"] = np.nan # initialization of the whole column at NaN
+
+  data["position"] = np.where(data["Close"]>data["Open"],1, data["position"])#data["position"]) #,data["position"])
+  #data["position"] = data["position"].ffill().fillna(0)
+  print(data)
+  return calcReturns(data)
+
+
+
+
+# Here are a few helper functions to give us returns and summary stats
+def calcReturns(df):
+  df['returns'] = df['Close'] / df['Close'].shift(1)
+  print(df)
+  df['log_returns'] = np.log(df['returns'])
+  df['strat_returns'] = df['position'].shift(1) * df['returns']
+  print(df)
+  df['strat_log_returns'] = df['position'].shift(1) * \
+      df['log_returns']
+  df['cum_returns'] = np.exp(df['log_returns'].cumsum()) - 1
+  df['strat_cum_returns'] = np.exp(
+      df['strat_log_returns'].cumsum()) - 1
+  df['peak'] = df['cum_returns'].cummax()
+  df['strat_peak'] = df['strat_cum_returns'].cummax()
+  return df
+
+def getStratStats(log_returns: pd.Series,
+  risk_free_rate: float = 0.02):
+  stats = {}  # Total Returns
+  stats['tot_returns'] = np.exp(log_returns.sum()) - 1  
+  
+  # Mean Annual Returns
+  stats['annual_returns'] = np.exp(log_returns.mean() * 252) - 1  
+  
+  # Annual Volatility
+  stats['annual_volatility'] = log_returns.std() * np.sqrt(252)
+  
+  # Sortino Ratio
+  annualized_downside = log_returns.loc[log_returns<0].std() * \
+    np.sqrt(252)
+  stats['sortino_ratio'] = (stats['annual_returns'] - \
+    risk_free_rate) / annualized_downside  
+  
+  # Sharpe Ratio
+  stats['sharpe_ratio'] = (stats['annual_returns'] - \
+    risk_free_rate) / stats['annual_volatility']  
+  
+  # Max Drawdown
+  cum_returns = log_returns.cumsum() - 1
+  peak = cum_returns.cummax()
+  drawdown = peak - cum_returns
+  max_idx = drawdown.argmax()
+  stats['max_drawdown'] = 1 - np.exp(cum_returns[max_idx]) \
+    / np.exp(peak[max_idx])
+  
+  # Max Drawdown Duration
+  strat_dd = drawdown[drawdown==0]
+  strat_dd_diff = strat_dd.index[1:] - strat_dd.index[:-1]
+  strat_dd_days = strat_dd_diff.map(lambda x: x.days).values
+  strat_dd_days = np.hstack([strat_dd_days,
+    (drawdown.index[-1] - strat_dd.index[-1]).days])
+  stats['max_drawdown_duration'] = strat_dd_days.max()
+  return {k: np.round(v, 4) if type(v) == np.float_ else v
+          for k, v in stats.items()}
+
+
+midDon = strategy(data.copy(), 10, shorts=False)
+
+plt.figure(figsize=(12, 4))
+plt.plot(midDon["strat_cum_returns"] * 100, label="Strategy")
+plt.plot(midDon["cum_returns"] * 100, label="Buy and Hold")
+plt.title("Cumulative Returns for Mid Donchian Cross-Over Strategy")
+plt.xlabel("Date")
+plt.ylabel("Returns (%)")
+plt.xticks(rotation=45)
+plt.legend()
+
+plt.show()
+
+stats = pd.DataFrame(getStratStats(midDon["log_returns"]), 
+                     index=["Buy and Hold"])
+stats = pd.concat([stats,
+                   pd.DataFrame(getStratStats(midDon["strat_log_returns"]),
+                               index=["MidDon X-Over"])])
+stats
+
+
+
+
+"""
+
+40-50k par an
+"""
