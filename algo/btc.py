10932469f3af673fcf3b9422ffa56b348e8c5877
AlexandreLeonetti <al.leo@wanadoo.fr>
2023-10-28 19:20:06 +0200

    add

--- algo/btc.py
+++ algo/btc.py
@@ -10,39 +10,36 @@ from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
 
-def indicator(data: pd.DataFrame, period: int):
-      return data
+#def indicator(data: pd.DataFrame, period: int):
+#      return data
 
 #downloading the correct data from yahoo finance
 ticker = "BTC-USD"
 yfObj = yf.Ticker(ticker) #returns data as panda dataframe.
-data = yfObj.history(start="2023-09-01", end="2023-10-25").drop(
+data = yfObj.history(start="2021-01-01", end="2021-10-25").drop(
             ["Volume", "Stock Splits"], axis=1)
-data = indicator(data, 10)
-#print(data)
+#data = indicator(data, 10)
 data.tail()
 
-print("*************")
+#******************************************
+
 df =data
-#print(df)
-stop_percent = 0.993 #ratio value
+stop_percent = 0.990 #ratio value
 df["ratio"]=df["Close"]/df["Open"]
 df["Stop"]=df["Open"]*stop_percent
 # Set "selected" column based on the condition
-#df.loc[df["ratio"] >= 0.999, "returns"] = df["ratio"]
 df.loc[(df["Low"] >= df["Stop"]), "returns"] = df["ratio"] # need to add drawdawn
 
 # For rows where the condition is not met, set "selected" to 1.05
 df["returns"].fillna(stop_percent, inplace=True)
 n_bins = 500
 
-# Generate two normal distributions
+# distribution 1 is returns 
 dist1 = df["returns"]
 fig, axs = plt.subplots(1,  tight_layout=True)
 
 # We can set the number of bins with the *bins* keyword argument.
 plt.hist(dist1, bins=n_bins)
-#axs[1].hist(dist2, bins=n_bins)
 
 # Add a vertical line at x = 1
 plt.axvline(x=1, color='red', linestyle='--', label='x=1')
@@ -65,4 +62,6 @@ plt.xticks(rotation=45)
 plt.show()
 """
 returns from oct 01 to oct 24 included is 1.257
+node js robot server
+https://www.youtube.com/watch?v=jaOIzY3UK3k
 """
